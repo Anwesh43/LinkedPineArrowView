cde386035fec9751c69409563b3b1c3468595396
Anwesh43 <anweshthecool0@gmail.com>
2019-01-28 00:46:09 +0530

    importing libraries and defining constants

--- pinearrowview/src/main/java/com/anwesh/uiprojects/pinearrowview/PineArrowView.kt
+++ pinearrowview/src/main/java/com/anwesh/uiprojects/pinearrowview/PineArrowView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.pinearrowview
 /**
  * Created by anweshmishra on 28/01/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.05f
+val lastStep : Int = 2
+val scDiv : Double = 0.51
+val foreColor : Int = Color.parseColor("#4527A0")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val sizeFactor : Float = 2.9f
+val strokeFactor : Int = 90
