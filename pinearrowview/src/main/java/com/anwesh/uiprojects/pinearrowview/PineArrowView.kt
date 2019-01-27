3c83646f7927f79e94f237d81610d9e5314433de
Anwesh43 <anweshthecool0@gmail.com>
2019-01-28 01:03:05 +0530

    adding PineArrowView class

--- pinearrowview/src/main/java/com/anwesh/uiprojects/pinearrowview/PineArrowView.kt
+++ pinearrowview/src/main/java/com/anwesh/uiprojects/pinearrowview/PineArrowView.kt
@@ -61,3 +61,21 @@ fun Canvas.drawPANode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class PineArrowView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
